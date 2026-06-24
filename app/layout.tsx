4b9461434e327d15fa8fea6e0dced1d473cbd31c
shion katakura <shionshionshion5101@gmail.com>
2026-06-24 13:01:36 +0900

    feat: GA4計測タグを追加 (G-JDW1QX1NR2)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Space_Grotesk, JetBrains_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const spaceGrotesk = Space_Grotesk({
@@ -49,6 +50,7 @@ export default function RootLayout({
         />
       </head>
       <body>{children}</body>
+      <GoogleAnalytics gaId="G-JDW1QX1NR2" />
     </html>
   );
 }
